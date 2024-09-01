d12e1654e4da7f60c55b851ab4db869c2c253d60
river0203 <aspp3666@gmail.com>
2024-09-01 12:29:51 +0900

    border update

--- MainVeiw.java
+++ MainVeiw.java
@@ -1,12 +1,36 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.event.*;
 
 public class MainVeiw extends JPanel{
+
+    private Controller  experController;
+    private JPanel      menuPanel, selectPanel, performancePanel;
+
     public MainVeiw()
     {
         setBackground(Color.white);
         setPreferredSize(new Dimension(1280, 800));
         setLayout(null);
+
+        menuPanel = new JPanel();
+        menuPanel.setBounds(0, 0, 1280, 80);
+        menuPanel.setBackground(Color.white);
+        menuPanel.setBorder(BorderFactory.createTitledBorder("Menu"));
+        menuPanel.setLayout(new GridLayout(1, 4));
+        add(menuPanel);
+
+        selectPanel = new JPanel();
+        selectPanel.setBounds(0, 85, 250, 700);
+        selectPanel.setBackground(Color.white);
+        selectPanel.setBorder(BorderFactory.createTitledBorder("Select"));
+        add(selectPanel);
+
+        performancePanel = new JPanel();
+        performancePanel.setBounds(980, 85, 300, 700);
+        performancePanel.setBackground(Color.white);
+        performancePanel.setBorder(BorderFactory.createTitledBorder("Performance"));
+        add(performancePanel);
     }
 }
