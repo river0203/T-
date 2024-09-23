a4d16b3810bcad867c39963dba18525e8d033336
river0203 <aspp3666@gmail.com>
2024-09-23 20:46:17 +0900

    Revert "func img print error"
    This reverts commit 7b1335b1acbe310fbbf60d532ade3724be9d7737.

--- Controller.java
+++ Controller.java
@@ -1,18 +1,30 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 
 public class Controller extends JPanel {
 
-    private MainVeiw view;
+    private BufferedImage image;
+
+    // MainVeiw로부터 JPanel을 받는 생성자
+    public Controller(MainVeiw mainVeiw) {
+        try {
+            // 이미지 파일 로드 (파일 경로를 절대경로 또는 상대경로로 수정)
+            image = ImageIO.read(new File("/Users/iseungbin/Documents/GitHub/T-/ImageResource/BackGround.jpg"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-    }
 
-    public Controller(MainVeiw v)
-    {
-        view = v;
-        setBackground(Color.white);
+        // 이미지가 있을 때만 그리기
+        if (image != null) {
+            g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+        }
     }
 }
