d25a05785f328ed02d9bb3f676d31cce35158018
river0203 <aspp3666@gmail.com>
2025-01-05 00:16:18 +0900

    Feat : Button Mapping

--- Contants.java
+++ Contants.java
@@ -12,15 +12,16 @@ public class Contants {
 
     static final public int LOGO    = 0;
     static final public int UNDO    = 1;
-    static final public int REDO    = 2;
-    static final public int CLEAR   = 3;
+    static final public int FOWARD    = 2;
+    static final public int BACK   = 3;
+    static final public int TURN    = 4;
 
     //circuit btn mapping
-    static final public int LINE    = 4;
-    static final public int BATTERY = 5;
-    static final public int BATHOLD = 6;
-    static final public int SWITCH  = 7;
-    static final public int MOTOR   = 8;
-    static final public int REGIS   = 9;
-    static final public int WING    = 10;
+    static final public int LINE    = 5;
+    static final public int BATTERY = 6;
+    static final public int BATHOLD = 7;
+    static final public int SWITCH  = 8;
+    static final public int MOTOR   = 9;
+    static final public int REGIS   = 10;
+    static final public int WING    = 11;
 }
--- MainVeiw.java
+++ MainVeiw.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.event.MenuListener;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.*;
 import java.awt.event.*;
@@ -8,12 +9,68 @@ import java.util.Stack;
 
 public class MainVeiw extends JPanel {
 
-
+    private JPanel menuPanel;
+    private JButton[] btnMenuArray;
 
     public MainVeiw() {
         setBackground(Color.white);
         setPreferredSize(new Dimension(1280, 800));
         setLayout(null);
+
+        menuPanel = new JPanel();
+        menuPanel.setBounds(10, 10, 1260, 100);
+        menuPanel.setBackground(Color.white);
+        menuPanel.setBorder(BorderFactory.createTitledBorder("menu"));
+        menuPanel.setLayout(new GridLayout(1, 5));
+        add(menuPanel);
+
+        btnMenuArray = new JButton[5];
+        for(int i = 0; i < 5; i++)
+        {
+            btnMenuArray[i] = new JButton(Contants.BUTTON[i]);
+            btnMenuArray[i].setBackground(Contants.HOVERING[0]);
+            btnMenuArray[i].setForeground(Contants.HOVERING[1]);
+            btnMenuArray[i].addMouseListener(new HoveringListener());
+            btnMenuArray[i].addActionListener(new MenuListener());
+            menuPanel.add(btnMenuArray[i]);
+        }
     }
 
+    private class HoveringListener implements MouseListener
+    {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
+
+    private class MenuListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object obj = e.getSource();
+        }
+    }
 }
