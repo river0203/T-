75d3121ea480be7a072c08657a91455e8d4def0a
river0203 <aspp3666@gmail.com>
2025-01-09 22:55:40 +0900

    chore : Logic getter/setter

--- Controller.java
+++ Controller.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-//TODO controller 브렌치, 충돌 구현
+//TODO controller 브렌치, 충돌 구현, 전선 그리기 버튼 누르면 lab 화면에 이미지 띄우기, 띄운 이미지 컨트롤 하기
+
 
 public class Controller extends JPanel {
     private MainView view;
@@ -12,6 +13,15 @@ public class Controller extends JPanel {
     public Controller(MainView v)
     {
 
+    }
+
+    public int getPresentLogic()
+    {
+        return nowData.presentLogic;
+    }
+    public void setPresentLogic(int nChangeLogic)
+    {
+        nowData.presentLogic = nChangeLogic;
     }
 
 }
