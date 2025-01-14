2c67e9348acfb97ee0649bafc0e8991e315e8cbe
river0203 <aspp3666@gmail.com>
2025-01-14 22:13:21 +0900

    no message

--- LogicEdu.java
+++
@@ -1,5 +0,0 @@
-public interface LogicEdu {
-
-    //캔디 크러시 사가처럼 스왑 확인 - > 회로에 고유의 숫자 부여(회로와 빵판의 숫자가 일치하면 퍼즐정답)
-    public abstract void checkMatchingBoard();
-}
---
+++ LogicTool.java
@@ -0,0 +1,18 @@
+
+
+public abstract class LogicTool {
+
+    public static final int plusPole = 1;
+    public static final int minusPole = 1;
+
+    public boolean matchingPoles()
+    {
+        /*if()
+        {
+            return true;
+        }
+         */
+
+        return false;
+    }
+}
