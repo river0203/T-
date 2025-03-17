844c4cd86d253159520d08af845db3bbfc3965c5
river0203 <aspp3666@gmail.com>
2025-03-17 19:41:44 +0900

    chore : 이하 동문

--- LogicTool.java
+++ LogicTool.java
@@ -1,7 +1,7 @@
 
 
 public abstract class LogicTool {
-
+    //캔디 크러시 사가처럼 스왑 확인 - > 회로에 고유의 숫자 부여(회로와 빵판의 숫자가 일치하면 퍼즐정답)
     public static final int plusPole = 1;
     public static final int minusPole = 1;
 
--- Node.java
+++ Node.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+
+    // node의 특징 파워 1, 접지 2, 연결부 3
+
+    //private int voltage = 0;
+    private boolean isPower = false;
+    private boolean isGround = false;
+    private boolean isConnection = false;
+    private boolean isComponentOn = false;
+    private int[] node = new int[5];
+
+    public Node()
+    {
+
+    }
+
+    public Node(int nodeMode)
+    {
+
+    }
+
+    public boolean getPower(){return isPower;}
+    public void setPower(boolean changePower){isPower = changePower;}
+    public boolean getGround(){return isGround;}
+    public void setGround(boolean changeGround){isGround = changeGround;}
+    public boolean getConnection(){return isConnection;}
+    public void setConnection(boolean changeConnection){isConnection = changeConnection;}
+
+    public void changeNodeMode(int nodeMode)
+    {
+        switch (nodeMode)
+        {
+            case 1:
+                isPower = true;
+                break;
+
+            case 2:
+                isGround = true;
+                break;
+
+            case 3:
+                isConnection = true;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void removeComponet()
+    {
+
+    }
 }
